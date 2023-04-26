42b42055dea8593a3b9882c8238d29fb82baa359
gabriel <gabriel.lima2307@uol.com.br>
2023-04-26 13:24:48 -0300

    finish

--- projeto_global/src/router.js
+++ projeto_global/src/router.js
@@ -1,14 +1,20 @@
-import { RouterLink } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+// import { RouterLink } from 'vue-router'
 
-import MyService from './home/MyService.vue'
+// import MyHero from './views/MyHero.vue'
+// import MyService from './views/MyService.vue'
+// import MyDepoiment from './views/MyDepoiment'
+// import MyAbout from './views/MyAbout.vue'
+// import MyCliente from './views/MyCliente.vue'
 
-const routes = {
+// const routes = [
     
-    {
-        path: '/service',
-        name: 'Mentoria',
-        component: Services,
-        //Permitindo que todos os parametros da rota sejam passados como atributos
-        props:true,
-    },
-}
+//     {
+//         path: '/service',
+//         name: 'Mentoria',
+//         component: Services,
+//         //Permitindo que todos os parametros da rota sejam passados como atributos
+//         props:true,
+//     },
+
+// ]
