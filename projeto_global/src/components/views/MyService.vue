0037b1075819b59caae7696584c5b931c789038d
gabriel <gabriel.lima2307@uol.com.br>
2023-05-03 23:53:01 -0300

    correção enquadramento

--- projeto_global/src/components/views/MyService.vue
+++ projeto_global/src/components/views/MyService.vue
@@ -18,52 +18,48 @@
                     </div>
                 </div>
                 <div class="text">
-                    <p><span>- Adriana Souza</span>, 36 anos e 20 anos de mercado de trabalho.<br>
+                    <p><span>Adriana Souza</span>, 36 anos e 20 anos de mercado de trabalho.
 
-                        <br>Esteve como vendedora interna/externa, representante comercial, modelo, instrutora de informática/digitação, instrutora de cursos profissionalizantes, projetista e outras experiências avulsas.<br>
+                        Esteve como vendedora interna/externa, representante comercial, modelo, instrutora de informática/digitação, instrutora de cursos profissionalizantes, projetista e outras experiências avulsas.
                         
-                        <br>Formou-se em administração de empresas pela UFRN em 2010, totalmente por amor a profissão. Fez a formação em mentoring para poder exercer a profissão da maneira correta além disso possui MBA e diversos cursos de extensão em finanças, marketing e negócios. <br>
+                        Formou-se em administração de empresas pela UFRN em 2010, totalmente por amor a profissão. Fez a formação em mentoring para poder exercer a profissão da maneira correta além disso possui MBA e diversos cursos de extensão em finanças, marketing e negócios. 
                         
-                        <br>É idealizadora, treinadora e mentora empresarial na @global , esta empresa que recentemente completou quatorze anos no mercado e quem nos acompanha sabe que são anos incansáveis em que nunca paramos de inovar e evoluir com o propósito de impactar a vida pessoas e negócios.<br>
-                        
-                        <br>Esteve por nove anos envolvida em diretorias de CDL’s e FCDL/RN, durante um ano estive como conselheira do Sebrae/RN.<br>
-                        
-                        <br>Neste mundo dos negócios está sócia na startup @digai o qual tem como objetivo fortalecer a gestão de relacionamento empresa - clientes.<br>
-                        
-                        <br>Atualmente está como embaixadora da @redemulher que tem como objetivo levar o protagonismo feminino para dentro das empresas.<br>
-                        
-                        <br>É uma apaixonada por pessoas e negócios, diariamente estuda, envolve-se e busca conhecimentos para ser a sua melhor versão e poder contribuir com a história do máximo de pessoas possível.</p>
+                        É idealizadora, treinadora e mentora empresarial na @global , esta empresa que recentemente completou quatorze anos no mercado e quem nos acompanha sabe que são anos incansáveis em que nunca paramos de inovar e evoluir com o propósito de impactar a vida pessoas e negócios.
+                        Esteve por nove anos envolvida em diretorias de CDL’s e FCDL/RN, durante um ano estive como conselheira do Sebrae/RN.
+                        Neste mundo dos negócios está sócia na startup @digai o qual tem como objetivo fortalecer a gestão de relacionamento empresa - clientes.
+                        Atualmente está como embaixadora da @redemulher que tem como objetivo levar o protagonismo feminino para dentro das empresas.
+                        É uma apaixonada por pessoas e negócios, diariamente estuda, envolve-se e busca conhecimentos para ser a sua melhor versão e poder contribuir com a história do máximo de pessoas possível.</p>
 
-                    <p>Eu sou <span>Dayanna Soares</span>, 33 anos, e há 18 anos estou no mercado de trabalho.<br>
+                    <p>Eu sou <span>Dayanna Soares</span>, 33 anos, e há 18 anos estou no mercado de trabalho.
 
-                        <br>Vivi muitas experiências enriquecedoras para muita jornada profissional, atuei como vendedora, caixa, instrutora de cursos profissionalizantes, recrutadora de talentos para o mercado de trabalho, estagiária na área administrativa, professora de faculdade em curso de Administração e Gestão de Pessoas, e outras experiências mais que puderam compor a profissional que sou.<br>
+                        Vivi muitas experiências enriquecedoras para muita jornada profissional, atuei como vendedora, caixa, instrutora de cursos profissionalizantes, recrutadora de talentos para o mercado de trabalho, estagiária na área administrativa, professora de faculdade em curso de Administração e Gestão de Pessoas, e outras experiências mais que puderam compor a profissional que sou.
                         
-                        <br>Minha primeira graduação foi em Administração de Empresas pela UFRN, e na sequência realizei pós em Gestão de Pessoas e Negócios, formações na área de Coach e desenvolvimento humano pessoal e profissional, cursos de formação em liderança, e a formação em Psicanálise Clínica. Sempre apaixona por negócios e pela evolução profissional e pessoal das pessoas, me dedico com responsabilidade e na busca de muitos conhecimentos no que faço.<br>
+                        Minha primeira graduação foi em Administração de Empresas pela UFRN, e na sequência realizei pós em Gestão de Pessoas e Negócios, formações na área de Coach e desenvolvimento humano pessoal e profissional, cursos de formação em liderança, e a formação em Psicanálise Clínica. Sempre apaixona por negócios e pela evolução profissional e pessoal das pessoas, me dedico com responsabilidade e na busca de muitos conhecimentos no que faço.
                         
-                        <br>Atualmente sou Mentora de Gestão de Pessoas e Liderança, Treinadora de Equipes, Psicanalista Clínica e Palestrante.<br>
+                        Atualmente sou Mentora de Gestão de Pessoas e Liderança, Treinadora de Equipes, Psicanalista Clínica e Palestrante.
                         
-                        <br>Sou idealizadora, treinadora e Mentora da @global_pen, esta empresa que está atuando há 14 anos no mercado, sempre mantendo o nosso propósito de impactar a vida das pessoas e os negócios.<br>
+                        Sou idealizadora, treinadora e Mentora da @global_pen, esta empresa que está atuando há 14 anos no mercado, sempre mantendo o nosso propósito de impactar a vida das pessoas e os negócios.
                         
-                        <br>Atuando também como sócia na startup @appdigai o qual tem o objetivo de fortalecer a gestão de relacionamento entre empresas e clientes.<br>
+                        Atuando também como sócia na startup @appdigai o qual tem o objetivo de fortalecer a gestão de relacionamento entre empresas e clientes.
                         
-                        <br>Sigo buscando conhecimentos e me atualizando para entregar o meu melhor, me considero apaixonada pelo que faço, e me realizo transformando a vida das pessoas e dos negócios.</p>
+                        Sigo buscando conhecimentos e me atualizando para entregar o meu melhor, me considero apaixonada pelo que faço, e me realizo transformando a vida das pessoas e dos negócios.</p>
                     
-                    <p>Eu sou <span>Kelciano Douglas</span>, 23 anos de mercado de trabalho.<br>
-                        <br>Multidisciplinar, me formei em FARMÁCIA no ano de 1999 na UEPB e em ANÁLISES CLÍNICAS (Bioquímica) em 2000.<br>
+                    <p>Eu sou <span>Kelciano Douglas</span>, 23 anos de mercado de trabalho.
+                        Multidisciplinar, me formei em FARMÁCIA no ano de 1999 na UEPB e em ANÁLISES CLÍNICAS (Bioquímica) em 2000.
                         
-                        <br>Estive com Farmacêutico e Bioquímico nos setores público e privado por quase 20 anos.<br>
+                        Estive com Farmacêutico e Bioquímico nos setores público e privado por quase 20 anos.
                         
-                        <br>Nesse tempo me formei em ADMINISTRAÇÃO DE EMPRESAS pela UFRN (2005) e fiz MBA em Gestão Financeira pela FGV/RJ em 2007.<br>
+                        Nesse tempo me formei em ADMINISTRAÇÃO DE EMPRESAS pela UFRN (2005) e fiz MBA em Gestão Financeira pela FGV/RJ em 2007.
                         
-                        <br>Tenho formação em COACHING pelo IBC (2017) e especialização em MARKETING DIGITAL e LANÇAMENTOS DIGITAIS.<br>
+                        Tenho formação em COACHING pelo IBC (2017) e especialização em MARKETING DIGITAL e LANÇAMENTOS DIGITAIS.
                         
-                        <br>Na @global_pen exerço a função de CMO e faço a gestão do braço digital da consultoria.<br>
+                        Na @global_pen exerço a função de CMO e faço a gestão do braço digital da consultoria.
                         
-                        <br>Na @global_agd exerço a função de CEO e faço a gestão de vendas, onde atendemos uma carteira com mais de 100 clientes dos mais diversos segmentos em diferentes estados do Brasil e em países como EUA, Portugal, Chile e Itália.<br>
+                        Na @global_agd exerço a função de CEO e faço a gestão de vendas, onde atendemos uma carteira com mais de 100 clientes dos mais diversos segmentos em diferentes estados do Brasil e em países como EUA, Portugal, Chile e Itália.
                         
-                        <br>Nosso maior objetivo na Global é contribuir com a realização dos sonhos dos nossos clientes.<br>
+                        Nosso maior objetivo na Global é contribuir com a realização dos sonhos dos nossos clientes.
                         
-                        <br>Sou entusiasta da tecnologia e busco o aperfeiçoamento contínuo para trazer as melhores e mais modernas soluções para os nossos clientes.</p>
+                        Sou entusiasta da tecnologia e busco o aperfeiçoamento contínuo para trazer as melhores e mais modernas soluções para os nossos clientes.</p>
                 </div>
             
             </div>
@@ -86,7 +82,8 @@ export default {
         display: flex;
         flex-direction: column;
         align-items: center;
-        padding: 100px 138px 0px 138px;
+        justify-content: center;
+        padding: 100px 138px 0px;
         gap: 50px;
         background-color: #B4B3B3;
 
@@ -104,8 +101,9 @@ export default {
         display: flex;
         flex-direction: row;
         align-items: center;
-        padding: 0px 40px;
-        gap: 250px;
+        justify-content: center;
+        padding: 0px 400px;
+        gap: 100px;
     }
 
     .divisao{
@@ -131,15 +129,16 @@ export default {
         flex-direction: column;
         align-items: flex-start;
         padding: 0px;
-        gap: 370px;
+        gap: 430px;
     }
 
     .img{
         display: flex;
         flex-direction: column;
+        justify-content: flex-start;
         align-items: flex-start;
-        padding: 40px 0px 100px;
-        gap: 150px;
+        /* padding: 0px 0px 100px; */
+        gap: 200px;
         width: 300px;
 
     }
@@ -206,13 +205,13 @@ export default {
         display: flex;
         flex-direction: column;
         align-items: flex-start;
-        gap: 100px;
-
+        gap: 250px;
+        text-align: justify;
         font-family: 'Inter';
         font-style: normal;
         font-weight: 400;
         font-size: 1rem;
-        line-height: 100%;
+        line-height: 20px;
 
         color: #FFFFFF;
         padding: 0px;
@@ -224,12 +223,6 @@ export default {
         font-weight: bold;
         font-size: 1rem;
     }
-
-    p{
-        padding-bottom: 40px;
-        border-bottom: 1px solid white;
-    }
-
 
 
     
