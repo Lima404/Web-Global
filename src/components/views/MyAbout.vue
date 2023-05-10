4a4fbcebda9c21d240514341adf1f065eb590de7
gabriel <gabriel.lima2307@uol.com.br>
2023-05-10 18:16:26 -0300

    responsivity about

--- src/components/views/MyAbout.vue
+++ src/components/views/MyAbout.vue
@@ -1,39 +1,44 @@
-<template lang="pt-br">
+    <template lang="pt-br">
     <section class="about-container">
         <div class="container about">
             <div class="base">
                 <div class="buttons">
-                    <p class="but">VALORES</p>
-                    <p class="but">VISÃO</p>
-                    <p class="but">MISSÃO</p>
-                </div>
-                <div class="box">
-                    <div class="linha">
-                        <p class="text">Comprometimento, humanização, responsabilidade, transparência e ética.</p>
-                        <p class="text">Ser referência como uma agência completa em serviços empresariais.</p>
-                        <p class="text">Impactar positivamente a vida de pessoas e de negócios.</p>
-                    </div>
-                    <p class="but">HISÓRIA</p>
-                    <div class="hist">
-                        <div>
-                            <p class="boxh">A Global surgiu a partir de um trabalho da faculdade de administração na UFRN, quando a CEO Adriana Souza juntamente com colegas criaram uma Empresa de Consultoria – A GLOBAL.<br><br>
-
-                                O que surgiu como um trabalho acadêmico logo se tornou realidade quando a então aluna de administração, começou a realizar serviços no sentido de contribuir com o sucesso de pessoas e negócios.<br><br>
-                                
-                                Logo em seguida a empresa foi formalizada e passou a atuar em todo o Rio Grande do Norte atendendo empresas dos mais diversos segmentos.<br><br>
-                                
-                                Em 2019, Adriana fez a formação em Mentoring impulsionando ainda mais os resultados dos clientes ao utilizar ferramentas modernas e extraem todo o potencial empreendedor de pessoas dedicadas aos seus negócios.<br><br>
-                                
-                                Hoje a Global é formada pelas áreas de gestão, marketing e finanças com Adriana Souza à frente; Dayanna Soares com liderança, gestão de pessoas, treinamentos e psicanálise;  a área de marketing digital e design gráfico com Kelciano Douglas.<br><br>
-                                
-                                A Global é referência em todo o Nordeste quando se fala em Consultoria, Treinamentos, Gestão e Mentoria para empresas de pequeno/médio porte, profissionais liberais e gestores.
-                            </p>
+                    <div class="box">
+                        <div class="linha">
+                            <div class="card">
+                                <p class="but">VALORES</p>
+                                <p class="text1">Comprometimento, humanização, responsabilidade, transparência e ética.</p>
+                            </div>
+                            <div class="card">
+                                <p class="but">VISÃO</p>
+                                <p class="text">Ser referência como uma agência completa em serviços empresariais.</p>
+                            </div>
+                            <div class="card">
+                                <p class="but">MISSÃO</p>
+                                <p class="text">Impactar positivamente a vida de pessoas e de negócios.</p>
+                            </div>
                         </div>
-                        
-                    </div>
-                </div>
-            </div>
+                        <p class="but">HISÓRIA</p>
+                        <div class="hist">
+                            <div>
+                                <p class="boxh">A Global surgiu a partir de um trabalho da faculdade de administração na UFRN, quando a CEO Adriana Souza juntamente com colegas criaram uma Empresa de Consultoria – A GLOBAL.
 
+                                    O que surgiu como um trabalho acadêmico logo se tornou realidade quando a então aluna de administração, começou a realizar serviços no sentido de contribuir com o sucesso de pessoas e negócios.
+                                    
+                                    Logo em seguida a empresa foi formalizada e passou a atuar em todo o Rio Grande do Norte atendendo empresas dos mais diversos segmentos.
+                                    
+                                    Em 2019, Adriana fez a formação em Mentoring impulsionando ainda mais os resultados dos clientes ao utilizar ferramentas modernas e extraem todo o potencial empreendedor de pessoas dedicadas aos seus negócios.
+                                    
+                                    Hoje a Global é formada pelas áreas de gestão, marketing e finanças com Adriana Souza à frente; Dayanna Soares com liderança, gestão de pessoas, treinamentos e psicanálise;  a área de marketing digital e design gráfico com Kelciano Douglas.
+                                    
+                                    A Global é referência em todo o Nordeste quando se fala em Consultoria, Treinamentos, Gestão e Mentoria para empresas de pequeno/médio porte, profissionais liberais e gestores.
+                                </p>
+                            </div>
+                            
+                        </div>
+                    </div>
+            </div>
+        </div>
             <div class="nichos">
                 <h2 id="cliente">NOSSOS CLIENTES</h2>
             </div>
@@ -49,7 +54,15 @@ export default {
 
     .about-container{
         background-color: #b4b3b3;
-        padding: 150px 70px 0px;
+        padding: 100px 70px 0px;
+    }
+
+    .card{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
     }
 
     h2{
@@ -108,11 +121,18 @@ export default {
     }
 
     .but{
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        
+        
         background: #E6C772;
         color: #002647;
         box-shadow: 0px 10px 15px rgba(136, 144, 194, 0.2), 0px 5px 15px rgba(37, 44, 97, 0.15);
         border-radius: 4px;
         padding: 20px 40px;
+        width: 100px;
+        
 
         font-family: 'inter';
         font-style: bold;
@@ -140,6 +160,15 @@ export default {
         width: 200px;
     }
 
+    .text1  {
+        background: #002647;
+        border-radius: 20px;
+        box-shadow: 0px 10px 15px rgba(136, 144, 194, 0.2), 0px 5px 15px rgba(37, 44, 97, 0.15);
+        text-align: center;
+        padding: 32px 30px;
+        width: 200px;
+    }
+
     .boxh{
         display: flex;
         flex-direction: column;
@@ -147,6 +176,7 @@ export default {
         border-radius: 20px;
         text-align: justify;
         margin-top: 10px;
+        line-height: 20px;
         padding: 40px 30px;
         width: 820px;
         gap: 50px;
@@ -178,6 +208,18 @@ export default {
         display: flex;
         gap: 50px;
         padding-bottom: 40px;
+    }
+
+    @media(max-width:910px){
+        
+        .linha{
+            display: flex;
+            flex-direction: column;
+        }
+
+        .boxh{
+            max-width: 500px;
+        }
     }
 
 
